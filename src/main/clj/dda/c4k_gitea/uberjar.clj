1252a4b49144c69a394efe960d26abd09f4cd82e
erik <erik.seiert@meissa-gmbh.de>
2022-09-08 15:04:17 +0200

    Project skeleton

---
+++ src/main/clj/dda/c4k_gitea/uberjar.clj
@@ -0,0 +1,9 @@
+(ns dda.c4k-website.uberjar
+  (:gen-class)
+  (:require
+   [dda.c4k-website.core :as core]
+   [dda.c4k-website.website :as website]
+   [dda.c4k-common.uberjar :as uberjar]))
+
+(defn -main [& cmd-args]
+  (uberjar/main-common "c4k-website" website/config? website/auth? website/config-defaults core/k8s-objects cmd-args))
