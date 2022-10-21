96be820b6cbee38973c673c2f1eb48688d446ce2
erik <erik.seiert@meissa-gmbh.de>
2022-10-21 12:15:28 +0200

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-website "0.1.2-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-website "0.1.2"
   :description "website c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
