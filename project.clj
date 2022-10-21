1d533a6491ee10259c1205baa46f47cda0060c15
erik <erik.seiert@meissa-gmbh.de>
2022-10-21 12:26:19 +0200

    [Skip-CI] Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-website "0.1.3"
+(defproject org.domaindrivenarchitecture/c4k-website "0.1.4-SNAPSHOT"
   :description "website c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
