11d12bc7d0cdda88abf124328b81205c0e438e76
erik <erik.seiert@meissa-gmbh.de>
2022-12-09 10:49:57 +0100

    Set up correct test for build image

--- infrastructure/c4k-website-build/image/resources/entrypoint.sh
+++ infrastructure/c4k-website-build/image/resources/entrypoint.sh
@@ -18,8 +18,8 @@ newHash=$( get-hash-data )
 if [[ $currentHash == $newHash ]]
     then
         echo "Nothing to do"
-    else        
-        write-hash-data $currentHash $hashfilename
+    else
+        echo $currentHash > $HASHFILEDIR/$hashfilename
         echo "Downloading website data"
         get-website-data $filename
         unzip-website-data $filename
