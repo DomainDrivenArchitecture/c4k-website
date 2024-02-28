a0bf9064ed95a44d4a7ed705a45fb9f97b4e312d
Michael Jerger <michael.jerger@meissa-gmbh.de>
2024-02-28 10:26:59 +0100

    update deps & fix tests

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.reader "1.4.0"]                 
-                 [org.domaindrivenarchitecture/c4k-common-clj "6.1.4-SNAPSHOT"]
+                 [org.domaindrivenarchitecture/c4k-common-clj "6.2.1"]
                  [hickory "0.7.1" :exclusions [viebel/codox-klipse-theme]]]
   :target-path "target/%s/"
   :source-paths ["src/main/cljc"
