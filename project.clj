0d2fe6938c924f799e11922c71499575dfcde5cb
Michael Jerger <michael.jerger@meissa-gmbh.de>
2025-05-14 10:53:37 +0200

    release: 2.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-website "2.0.4-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-website "2.1.0"
   :description "website c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
